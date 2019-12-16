c74e7895eb0d219d4007775eec134dd2bcd9d1ae
Alexey Milovidov <milovidov@yandex-team.ru>
2019-12-16 04:23:38 +0300

    Fixed error

--- crypto/sha/sha256.c
+++ crypto/sha/sha256.c
@@ -124,7 +124,6 @@ int SHA224_Final(unsigned char *md, SHA256_CTX *c)
 #define HASH_FINAL              SHA256_Final
 #define HASH_BLOCK_DATA_ORDER   sha256_block_data_order
 #ifndef SHA256_ASM
-static
 #endif
 void sha256_block_data_order(SHA256_CTX *ctx, const void *in, size_t num);
 
@@ -165,7 +164,7 @@ static const SHA_LONG K256[64] = {
 
 # ifdef OPENSSL_SMALL_FOOTPRINT
 
-static void sha256_block_data_order(SHA256_CTX *ctx, const void *in,
+void sha256_block_data_order(SHA256_CTX *ctx, const void *in,
                                     size_t num)
 {
     unsigned MD32_REG_T a, b, c, d, e, f, g, h, s0, s1, T1, T2;
@@ -243,7 +242,7 @@ static void sha256_block_data_order(SHA256_CTX *ctx, const void *in,
         T1 = X[(i)&0x0f] += s0 + s1 + X[(i+9)&0x0f];    \
         ROUND_00_15(i,a,b,c,d,e,f,g,h);         } while (0)
 
-static void sha256_block_data_order(SHA256_CTX *ctx, const void *in,
+void sha256_block_data_order(SHA256_CTX *ctx, const void *in,
                                     size_t num)
 {
     unsigned MD32_REG_T a, b, c, d, e, f, g, h, s0, s1, T1;
