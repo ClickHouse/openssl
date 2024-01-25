e695f2b83f19d418dc76a80a289c16237a77cfd3
Robert Schulze <robert@clickhouse.com>
2024-01-25 11:03:50 +0000

    Remove dependency on "getrandom" function
    (cherry picked from commit a6387b8f1883fc074cd92c83a20fa5930accaba6)

--- providers/implementations/rands/seeding/rand_unix.c
+++ providers/implementations/rands/seeding/rand_unix.c
@@ -351,8 +351,13 @@ static ssize_t syscall_random(void *buf, size_t buflen)
      * Note: Sometimes getentropy() can be provided but not implemented
      * internally. So we need to check errno for ENOSYS
      */
-#if !defined(__DragonFly__) && !defined(__NetBSD__) && !defined(__FreeBSD__)
-#if defined(__GNUC__) && __GNUC__ >= 2 && defined(__ELF__) && !defined(__hpux)
+#  if !defined(__DragonFly__) && !defined(__NetBSD__) && !defined(__FreeBSD__)
+
+     /// Disable the usage of "getentropy" function from libc (on static link time) to avoid dependency on too new libc version.
+     /// Otherwise, if we build ClickHouse on a system with new libc and run the built binary on a system with old libc, it will fail.
+     ///
+     /// Note that there is a fallback below to (1) runtime symbol lookup and (2) direct syscall, that are equivalent.
+#    if 0 && defined(__GNUC__) && __GNUC__>=2 && defined(__ELF__) && !defined(__hpux)
     extern int getentropy(void *buffer, size_t length) __attribute__((weak));
 
     if (getentropy != NULL) {
