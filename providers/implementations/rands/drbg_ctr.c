8d8691e7f4d8d01ae70145a16e507c69108402c7
Robert Schulze <robert@clickhouse.com>
2024-01-26 16:34:22 +0000

    Msan fixes

--- providers/implementations/rands/drbg_ctr.c
+++ providers/implementations/rands/drbg_ctr.c
@@ -24,6 +24,12 @@
 #include "crypto/evp/evp_local.h"
 #include "internal/provider.h"
 
+#if defined(__has_feature)
+# if __has_feature(memory_sanitizer)
+#  include <sanitizer/msan_interface.h>
+# endif
+#endif
+
 static OSSL_FUNC_rand_newctx_fn drbg_ctr_new_wrapper;
 static OSSL_FUNC_rand_freectx_fn drbg_ctr_free;
 static OSSL_FUNC_rand_instantiate_fn drbg_ctr_instantiate_wrapper;
@@ -71,6 +77,12 @@ static void inc_128(PROV_DRBG_CTR *ctr)
         p[n] = (u8)c;
         c >>= 8;
     } while (n);
+
+#if defined(__has_feature)
+# if __has_feature(memory_sanitizer)
+    __msan_unpoison(p, 16);
+# endif
+#endif
 }
 
 static void ctr_XOR(PROV_DRBG_CTR *ctr, const unsigned char *in, size_t inlen)
